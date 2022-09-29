6e287c7e7f6f2b7b6a2538cc3e0ecba710eaa19a
TanyaK <pctanya.kuo@gmail.com>
2022-09-29 22:10:06 +1000

    added and updated migrations, modified layout page and create admin blogs add blog

---
+++ BloggieWeb/Pages/Admin/Blogs/Add.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BloggieWeb.Pages.Admin.Blogs
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
