e2e5cbf5f34835e021ea38e30daacd5c3ec9a0bf
TanyaK <pctanya.kuo@gmail.com>
2022-09-29 18:20:02 +1000

    added models and connectionstring and dbcontext

---
+++ BloggieWeb/Models/Domain/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BloggieWeb.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public Guid BlogPostId { get; set; }
+    }
+}
+
