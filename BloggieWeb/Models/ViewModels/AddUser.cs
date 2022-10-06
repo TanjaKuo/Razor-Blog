4ecd52438caf269f43ede6317008810c47bcd99d
TanyaK <pctanya.kuo@gmail.com>
2022-10-06 17:35:02 +1100

    added validation for add new user from admin role

--- BloggieWeb/Models/ViewModels/AddUser.cs
+++ BloggieWeb/Models/ViewModels/AddUser.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace BloggieWeb.Models.ViewModels
 {
     public class AddUser
-    {
+    { 
+        [Required]
         public string Username{ get; set; }
+
+        [Required, EmailAddress]
         public string Email { get; set; }
+
+        [Required, MinLength(6)]
         public string Password { get; set; }
+
+        
         public bool AdminCheckbox { get; set; }
+
     }
 }
 
--- BloggieWeb/Pages/Admin/Users/Index.cshtml.cs
+++ BloggieWeb/Pages/Admin/Users/Index.cshtml.cs
@@ -1,86 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using BloggieWeb.Models.ViewModels;
-//using BloggieWeb.Repositories;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
-
-//namespace BloggieWeb.Pages.Admin.Users
-//{
-//    [Authorize(Roles = "Admin")]
-//    public class IndexModel : PageModel
-//    {
-//        private readonly IUserRepository _userRepository;
-
-//        // we don't want to show all the user identity, only a few, sp we create a viewmodel
-//        public List<User> Users { get; set; }
-
-//        [BindProperty]
-//        public AddUser AddUserRequest { get; set; }
-
-//        [BindProperty]
-//        public Guid SelectedUserId { get; set; }
-
-//        public IndexModel(IUserRepository userRepository)
-//        {
-//            _userRepository = userRepository;
-//        }
-
-
-//        public async Task<IActionResult> OnGet()
-//        {
-//            var users = await _userRepository.GetAll();
-
-//            Users = new List<User>();
-//            foreach (var user in users)
-//            {
-//                Users.Add(new Models.ViewModels.User()
-//                {
-//                    Id = Guid.Parse(user.Id),
-//                    Username = user.UserName,
-//                    Email = user.Email
-//                });
-//            }
-//            return Page();
-//        }
-
-//        public async Task<IActionResult> OnPost()
-//        {
-//            var identityUser = new IdentityUser
-//            {
-//                UserName = AddUserRequest.Username,
-//                Email = AddUserRequest.Email
-//            };
-
-//            var roles = new List<string> { "User" };
-//            if (AddUserRequest.AdminCheckbox)
-//            {
-//                roles.Add("Admin");
-//            }
-
-//            var result = await _userRepository.Add(identityUser, AddUserRequest.Password, roles);
-
-//            if (result)
-//            {
-//                return RedirectToPage("/Admin/Users/Index");
-//            };
-//            return Page();
-//        }
-
-
-//        public async Task<IActionResult> OnPostDelete()
-//        {
-//            await _userRepository.Delete(SelectedUserId);
-
-//            return RedirectToPage("/Admin/Users/Index");
-//        }
-//    }
-//}
-
+﻿
 using BloggieWeb.Models.ViewModels;
 using BloggieWeb.Repositories;
 using BloggieWeb.Models.ViewModels;
@@ -113,24 +31,16 @@ namespace BloggieWeb.Pages.Admin.Users
         public async Task<IActionResult> OnGet()
 
             {
-            var users = await _userRepository.GetAll();
-
-            Users = new List<User>();
-            foreach (var user in users)
-            {
-                Users.Add(new Models.ViewModels.User()
-                {
-                    Id = Guid.Parse(user.Id),
-                    Username = user.UserName,
-                    Email = user.Email
-                });
-            }
+            await GetUsers();
             return Page();
         }
 
         public async Task<IActionResult> OnPost()
         {
-           
+
+            if(ModelState.IsValid)
+            {
+
                 var identityUser = new IdentityUser
                 {
                     UserName = AddUserRequest.Username,
@@ -152,7 +62,10 @@ namespace BloggieWeb.Pages.Admin.Users
                 }
 
                 return Page();
-       
+            }
+            await GetUsers();
+            return Page();
+
         }
 
 
@@ -163,7 +76,21 @@ namespace BloggieWeb.Pages.Admin.Users
         }
 
 
-        
+        private async Task GetUsers()
+        {
+            var users = await _userRepository.GetAll();
+
+            Users = new List<User>();
+            foreach (var user in users)
+            {
+                Users.Add(new Models.ViewModels.User()
+                {
+                    Id = Guid.Parse(user.Id),
+                    Username = user.UserName,
+                    Email = user.Email
+                });
+            }
+        }
 
     }
 }
